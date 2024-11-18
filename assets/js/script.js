484efa771174e53bc0445152cf0dcb8856d8c20f
Bharathstline <ibharathkmp@gmail.com>
2024-11-18 19:18:54 +0400

    Initial commit with all project files

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// // Toggle the dropdown menu
+// const menuBtn = document.querySelector('.menu-btn');
+// const dropdownContent = document.querySelector('.dropdown-content');
+
+// menuBtn.addEventListener('click', () => {
+//     dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+// });
+
+// // Close the dropdown when clicking outside of it
+// document.addEventListener('click', (event) => {
+//     if (!dropdownContent.contains(event.target) && !menuBtn.contains(event.target)) {
+//         dropdownContent.style.display = 'none';
+//     }
+// });
